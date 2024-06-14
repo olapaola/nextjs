c61ca109e90ad9dd9905d0e6e145973ffc455dd8
Paola Garcia <paolagarcia@Paolas-MacBook-Pro.local>
2024-06-14 11:11:09 +0200

    Updated favicon icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,18 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Fundación Acción Neurodivergente",
   description: "Fundación Acción Neurodivergente",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", type: "image/png", sizes: "512x512" },
+  
+    ],
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+
+  },
 };
 
 export default function RootLayout({
